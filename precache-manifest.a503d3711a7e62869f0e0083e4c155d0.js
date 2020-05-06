46f2eabce522b380de37cbc73c34050c2bdb8274
Vincent Schroeder <vschroeder@ebay.com>
2020-05-06 20:52:03 +0200

    Updates

---
+++ precache-manifest.a503d3711a7e62869f0e0083e4c155d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1566b3ecdf363d57a07d526bcbfdd6",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c550377fe3d5d9ba6b53",
+    "url": "/memory-game/static/css/main.18d1de19.chunk.css"
+  },
+  {
+    "revision": "6ba6a9d32bbacb5cc9ea",
+    "url": "/memory-game/static/js/2.d83c6e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.d83c6e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c550377fe3d5d9ba6b53",
+    "url": "/memory-game/static/js/main.96967dc6.chunk.js"
+  },
+  {
+    "revision": "4e1481c7ed3ba6ba1a2e",
+    "url": "/memory-game/static/js/runtime-main.983dcba8.js"
+  },
+  {
+    "revision": "110eea8b9b6c0604cae3bad0d1acf723",
+    "url": "/memory-game/static/media/loader.110eea8b.svg"
+  }
+]);
